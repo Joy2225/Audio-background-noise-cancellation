4f354328a3121687ef8b552c9a09fc3464430d2c
mehulklh <106767917+mehulklh@users.noreply.github.com>
2024-11-20 17:54:51 +0530

    deletions

--- src/Wavelets/somethingtest.py
+++
@@ -1,9 +0,0 @@
-
-from something import AudioDeNoise 
-import time
-
-start=time.time()
-audioDenoiser = AudioDeNoise(inputFile="noisefunkguitare.wav")
-audioDenoiser.deNoise(outputFile="test_denoised.wav")
-end=time.time()
-print(f"Time taken: {end-start} seconds")
